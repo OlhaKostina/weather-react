439062659843217f198569495188652501f6d46f
OlhaKostina <105316483+OlhaKostina@users.noreply.github.com>
2022-09-15 18:16:28 +0300

    Added Footer component

---
+++ src/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <small>
+      <a
+        href="https://github.com/OlhaKostina/weather-forecast-app"
+        target="_blank norefferer"
+      >
+        Open-source code
+      </a>
+      <span> by Olha Kostina </span>
+    </small>
+  );
+}
