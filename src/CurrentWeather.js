c3bc6986dea98dac8f407deb2f8bc7cc1e3a53c4
OlhaKostina <105316483+OlhaKostina@users.noreply.github.com>
2022-12-22 10:29:04 +0200

    Update CurrentWeather.js

--- src/CurrentWeather.js
+++ src/CurrentWeather.js
@@ -22,7 +22,7 @@ export default function CurrentWeather(props) {
 
   function search() {
     const apiKey = "41db31b7bfb54ee46c6a15c5c9d02e0d";
-    let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(handleResponse);
   }
 
